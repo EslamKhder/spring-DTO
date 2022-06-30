0a7967626a11b45ddf9aa8db5b5fe55c601d710b
eslam khder <eslamkhder11@gmail.com>
2022-06-30 18:45:25 +0200

    create project dto

---
+++ src/main/java/com/spring/springdto/SpringDtoApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springdto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDtoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/springdto/SpringDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springdto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
